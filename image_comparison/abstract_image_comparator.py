3210fb3f462ef7df096733a403734244066445c5
gpawel <gpawel@gmail.com>
2024-11-28 19:53:27 -0500

    documented an method in the image comparison abstract root class.

--- image_comparison/abstract_image_comparator.py
+++ image_comparison/abstract_image_comparator.py
@@ -2,6 +2,12 @@ import numpy as np
 
 
 def check_image_loaded(img, image_path) -> np.ndarray:
+    """
+    This method checks if an image is loaded by provide path
+    :param img:
+    :param image_path:
+    :return: loaded image itself
+    """
     if img is None:
         raise ValueError("Error loading image from: " + image_path)
     return img
